f434fb5c9757c0b7e2003543013a045a69ea19ec
Nobby Knox <nobbyknox@gmail.com>
2016-05-02 18:44:55 +0200

    Add center text function and unit tests

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = function(text, width) {
+
+	if (!text) {
+		return '';
+	}
+
+	if (text.length >= width) {
+		return text;
+	}
+
+    let str = '';
+
+    for (let i = 0; i < ((width - text.length) / 2); i++) {
+        str += ' ';
+    }
+
+    str += text;
+    return str;
+}
+
---
+++ test/test.js
@@ -0,0 +1,35 @@
+var centerText = require('../index.js');
+
+var assert = require('assert');
+var path = require('path');
+
+describe(path.basename(__filename), function() {
+
+	it('should pad as expected', function() {
+		assert.equal(' abc', centerText('abc', 5));
+		assert.equal('  abc', centerText('abc', 6));
+		assert.equal('  abc', centerText('abc', 7));
+		assert.equal('   abc', centerText('abc', 8));
+		assert.equal('   abc', centerText('abc', 9));
+		assert.equal('    abc', centerText('abc', 10));
+		assert.equal('    abc', centerText('abc', 11));
+	});
+
+	it('should return original string when longer than width', function() {
+		assert.equal('abc', centerText('abc', 3));
+		assert.equal('abc', centerText('abc', 2));
+		assert.equal('abc', centerText('abc', 1));
+		assert.equal('abc', centerText('abc', 0));
+		assert.equal('abc', centerText('abc', -1));
+	});
+
+	it('should return empty on undefined', function() {
+		assert.equal('', centerText(undefined, 5));
+	});
+
+	it('should return empty on null', function() {
+		assert.equal('', centerText(null, 5));
+	});
+
+});
+
